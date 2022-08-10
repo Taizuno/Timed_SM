58dbff5019baac6daaf27a837312fc9a6df46cec
Braeden McNealy <lightning9501@gmail.com>
2022-08-10 18:34:27 -0400

    DbContext_B

--- AskTrevor.API/Program.cs
+++ AskTrevor.API/Program.cs
@@ -5,8 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen( c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "AskTrevor.API", Version = "v1"});
+    });
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
