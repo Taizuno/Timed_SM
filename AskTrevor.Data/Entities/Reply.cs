64f8dec2ddab0716002dd70f3ba02b7da03ea134
Braeden McNealy <lightning9501@gmail.com>
2022-08-10 20:09:29 -0400

    reply entity

---
+++ AskTrevor.Data/Entities/Reply.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AskTrevor.Data
+{
+    public class Reply
+    {
+        [Key]
+        public int Id {get; set;}
+        [Required]
+        public string Text {get; set;}
+        [Required]
+        public string UserName {get; set;}
+        [Required]
+        public DateTime Created {get; set;}
+        [Required]
+    }
+}
