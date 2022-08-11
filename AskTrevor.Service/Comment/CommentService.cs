60c5586c7339bf1ea1cb07f8ebf7c9857777f92c
Kristian Beall <beallkristian@gmail.com>
2022-08-11 18:13:48 -0400

    Added Comment create services to recognize a comment is being created and put it in the DB

--- AskTrevor.Service/Comment/CommentService.cs
+++ AskTrevor.Service/Comment/CommentService.cs
@@ -2,11 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AskTrevor.Data;
+using AskTrevor.Data.Entities;
+using AskTrevor.Models.Comment;
 
 namespace AskTrevor.Service.Comment
 {
     public class CommentService : ICommentService
     {
-        
+        private readonly AppDbContext _context;
+        public CommentService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CreateCommentAsync(CommentCreate model)
+        {
+            var entity = new CommentEntity
+            {
+                Id = model.Id,
+                Text = model.Text,
+                Username = model.Username,
+                CommentCreatedAt = DateTime.Now
+            };
+
+            _context.Comments.Add(entity);
+            var numberOfChanges = await _context.SaveChangesAsync();
+
+            return numberOfChanges == 1;
+        }
     }
 }
