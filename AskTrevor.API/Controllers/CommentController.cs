e7f25b18c163b27c447ad5fab216fa1a6ff8406d
Kristian Beall <beallkristian@gmail.com>
2022-08-10 21:28:33 -0400

    Added dependency injenction code for comment service in comment controller

--- AskTrevor.API/Controllers/CommentController.cs
+++ AskTrevor.API/Controllers/CommentController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using AskTrevor.Service.Comment;
 
 namespace AskTrevor.API.Controllers
 {
@@ -11,5 +12,11 @@ namespace AskTrevor.API.Controllers
     [ApiController]
     public class CommentController : ControllerBase
     {
+        private readonly ICommentService _commentService;
+        public CommentController(ICommentService service) {
+            {
+                _commentService = service;
+            }
+        }
     }
 }
