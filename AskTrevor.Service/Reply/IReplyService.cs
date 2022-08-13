55fc96d522960a6fe91d750ccd8baa531300d282
Braeden McNealy <lightning9501@gmail.com>
2022-08-13 08:46:09 -0400

    allmost

--- AskTrevor.Service/Reply/IReplyService.cs
+++ AskTrevor.Service/Reply/IReplyService.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AskTrevor.Models.Reply;
 
 namespace AskTrevor.Service.Reply
 {
     public interface IReplyService
     {
-        
+        Task<bool> CreateReplyAsync(ReplyCreate model);
     }
 }
